baac13a4759efca46ed24070562c0e634d0cb55f
Ben Oom <benjoom@gmail.com>
2020-05-21 11:34:12 -0400

    Initial commit

---
+++ source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/Employee.cs
@@ -0,0 +1,41 @@
+﻿/*Employee.cs
+ * Employee class with a AnnualSalary method
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ben_Oom_Wk3_Discussion
+{
+    class Employee
+    {
+     public string Name { get; set; } // auto-implemented property
+     private int annualSalary; //instance variable
+
+     public Employee(string firstName, string lastName, int monthlySalary)
+        {
+            Name = firstName + lastName;
+            AnnualSalary = monthlySalary;
+        }
+     
+     //AnnualSalary propery with validation
+     public int AnnualSalary
+        {
+            get
+            {
+                return annualSalary;
+            }
+            private set //only to be used within the class for validation
+            {
+                //validating the salary is greater than 0; if it is not, the instance variable
+                //annualSalary keeps its prior value
+                if (value > 0)
+                { 
+                    annualSalary = value * 12;
+                }
+            }
+        }
+    }
+}
---
+++ source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/EmployeeTest.cs
@@ -0,0 +1,30 @@
+﻿/* EmployeeTest.cs
+ * Exercise 4.11 - Store and display the first and last name of an employee, along with their monthly
+ * salary and annual salary. Utilizing Employee objects.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ben_Oom_Wk3_Discussion
+{
+    class EmployeeTest 
+    {
+        static void Main(string[] args)
+        {
+            //Initilize two Employee objects
+            Employee employee1 = new Employee("Bob", "Lovelife", 9000);
+            Employee employee2 = new Employee("Jane", "Olufsen", 4000);
+
+            //Display salary of each object
+            Console.WriteLine(
+                $"{employee1.Name}'s annual salary: {employee1.AnnualSalary:C}");
+            Console.WriteLine(
+                $"{employee2.Name}'s annual salary: {employee2.AnnualSalary:C}");
+
+
+        }
+    }
+}
