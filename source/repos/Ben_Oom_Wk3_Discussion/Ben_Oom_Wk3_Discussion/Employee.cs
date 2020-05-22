739330a36e36e55da19c130174cd33e78a4474e1
Ben Oom <benjoom@gmail.com>
2020-05-22 15:50:21 -0400

    completion of main method and additional adjustment method in Employee.cs

--- source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/Employee.cs
+++ source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/Employee.cs
@@ -14,28 +14,35 @@ namespace Ben_Oom_Wk3_Discussion
      public string Name { get; set; } // auto-implemented property
      private int annualSalary; //instance variable
 
+     // Employee constructor that receives three parameters
      public Employee(string firstName, string lastName, int monthlySalary)
         {
-            Name = firstName + lastName;
-            AnnualSalary = monthlySalary;
+            Name = firstName + lastName; // Concatenate the inputted name strings
+            AnnualSalary = monthlySalary; // AnnualSalary's set accessor validates the value
         }
      
-     //AnnualSalary propery with validation
+     // AnnualSalary propery with validation
      public int AnnualSalary
         {
             get
             {
                 return annualSalary;
             }
-            private set //only to be used within the class for validation
+            private set // only to be used within the class for validation
             {
-                //validating the salary is greater than 0; if it is not, the instance variable
-                //annualSalary keeps its prior value
+                // validating the salary is greater than 0; if it is not, the instance variable
+                // annualSalary keeps its prior value
                 if (value > 0)
                 { 
-                    annualSalary = value * 12;
+                    annualSalary = value * 12; // multiplying monthly value to achieve annual
                 }
             }
+        }
+
+     // method that can change the value of the AnnualSalary based on inputted adjustment to monthly
+     public void SalaryAdjustment(int adjustment)
+        {
+            AnnualSalary = adjustment;
         }
     }
 }
--- source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/EmployeeTest.cs
+++ source/repos/Ben_Oom_Wk3_Discussion/Ben_Oom_Wk3_Discussion/EmployeeTest.cs
@@ -14,17 +14,38 @@ namespace Ben_Oom_Wk3_Discussion
     {
         static void Main(string[] args)
         {
-            //Initilize two Employee objects
-            Employee employee1 = new Employee("Bob", "Lovelife", 9000);
-            Employee employee2 = new Employee("Jane", "Olufsen", 4000);
+            // Initilize two Employee objects
+            Employee employee1 = new Employee("Bob ", "Lovelife", 9000);
+            Employee employee2 = new Employee("Jane ", "Olufsen", 4000);
 
-            //Display salary of each object
+            // Display salary of each object
             Console.WriteLine(
                 $"{employee1.Name}'s annual salary: {employee1.AnnualSalary:C}");
             Console.WriteLine(
                 $"{employee2.Name}'s annual salary: {employee2.AnnualSalary:C}");
 
+            // Prompt for and read input of adjusted monthly salary amount
+            Console.Write($"\nEnter the new monthly salary for {employee1.Name}: ");
+            int adjustment = int.Parse(Console.ReadLine());
+            
+            // change the monthly salary amount for employee1
+            employee1.SalaryAdjustment(adjustment);
 
+            // Prompt for and read input of adjusted monthly salary amount
+            Console.Write($"Enter the new monthly salary for {employee2.Name}: ");
+            adjustment = int.Parse(Console.ReadLine());
+
+            // change the monthly salary amount for employee1
+            employee2.SalaryAdjustment(adjustment);
+
+            // display changed annual salary amounts
+            Console.WriteLine(
+                $"\n{employee1.Name}'s new salary is: {employee1.AnnualSalary:C}");
+            Console.WriteLine(
+                $"{employee2.Name}'s new salary is: {employee2.AnnualSalary:C}");
+
+            // stop console from closing
+            Console.ReadLine();
         }
     }
 }
